840e2d09e0b32bf65131fe5785e2fd98e35b3f9b
ivvaan@gmail.com <ivvaan@gmail.com@8a6e27c3-bfca-c8ce-82b8-d7b641a356f5>
2011-10-13 12:47:52 +0000

    replace tabs by spaces

--- utils.cpp
+++ utils.cpp
@@ -1,23 +1,23 @@
 /*
-	*
-	*      Copyright (c)  2011  Ivan Balaban 
-	*      ivvaan@gmail.com
-	*
-   This file is part of Seg_int library.
+*
+*      Copyright (c)  2011  Ivan Balaban 
+*      ivvaan@gmail.com
+*
+This file is part of Seg_int library.
 
-    Seg_int is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+Seg_int is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-    Seg_int is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+Seg_int is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with Seg_int.  If not, see <http://www.gnu.org/licenses/>.
-*/	
+You should have received a copy of the GNU General Public License
+along with Seg_int.  If not, see <http://www.gnu.org/licenses/>.
+*/ 
 #include "utils.h"
 
 #define PRIME1 33461
@@ -29,24 +29,24 @@
 
 double randm()
   {
-static int4 f1=MUL1;
-static int4 f2=MUL2;
-static int4 f3=MUL3;
-f1=(f1*MUL1)%PRIME1;
-f2=(f2*MUL2)%PRIME2;
-f3=(f3*MUL3)%PRIME3;
-return (double)f1+(double)f2*200.0+(double)f3*30000.0;
+  static int4 f1=MUL1;
+  static int4 f2=MUL2;
+  static int4 f3=MUL3;
+  f1=(f1*MUL1)%PRIME1;
+  f2=(f2*MUL2)%PRIME2;
+  f3=(f3*MUL3)%PRIME3;
+  return (double)f1+(double)f2*200.0+(double)f3*30000.0;
   };
 
 double CRandomValueGen::GetRandomDouble()
-	{
-		return randm();
-	}
+  {
+  return randm();
+  }
 
-	BOOL CRandomValueGen::RandomChoose()
-	{
-	  double r=randm();
-	  return randm()>r;
-	}
+BOOL CRandomValueGen::RandomChoose()
+  {
+  double r=randm();
+  return randm()>r;
+  }
 
-	
+
