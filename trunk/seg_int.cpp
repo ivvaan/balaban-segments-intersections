08912f9ae3307a1de603bd9ba1a7396379c7b6b6
ivvaan@gmail.com <ivvaan@gmail.com@8a6e27c3-bfca-c8ce-82b8-d7b641a356f5>
2012-08-15 12:53:01 +0000

    Bentley & Ottmann sweep line algorithm added. It doesn't work for nonline segments. Some minor rewriting and additional comments added.

--- trunk/seg_int.cpp
+++ trunk/seg_int.cpp
@@ -24,6 +24,7 @@ along with Seg_int.  If not, see <http://www.gnu.org/licenses/>.
 #include "test_coll.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <errno.h>
 
 
@@ -35,13 +36,15 @@ along with Seg_int.  If not, see <http://www.gnu.org/licenses/>.
 
 
 
-double counters[16];
-double _benchmark(int4 n,PSeg *coll,int4 s_type,int4 alg,double &res)
+double _benchmark(char *counters_string,int4 n,PSeg *coll,int4 s_type,int4 alg,double &res)
   {
   double start,stop;
+  double c[8];
   start=clock();
-  res=find_intersections(s_type,n,coll,alg,counters);
+  res=find_intersections(s_type,n,coll,alg,c);
   stop=clock();
+  if(counters_string)
+   sprintf(counters_string,"%13.0f;%13.0f;%13.0f;%13.0f;%13.0f;%13.0f;%13.0f;%13.0f",c[0],c[1],c[2],c[3],c[4],c[5],c[6],c[7]);
   return (stop-start)/CLOCKS_PER_SEC;
   }
 
@@ -49,20 +52,30 @@ double _benchmark(int4 n,PSeg *coll,int4 s_type,int4 alg,double &res)
 
 int main(int argc, char* argv[])
   {
-  int4 alg=31, seg_type=2, n = 20000, d=0;
+  int4 alg=63, seg_type=2, n = 20000, d=0;
   REAL p=1.0;
-  double exec_time[16],nInt[16];
+  double exec_time[33],nInt[33];
   char *ss="Lla",*sd="rmsp";
-  BOOL mute=FALSE;
-  //int4 *NN={,16000000}
+  BOOL mute=FALSE,print_counters=FALSE;
+  char counters_string[256],*pcs=NULL,*counters_mute;
 
 #ifdef _DEBUG
   _CrtSetDbgFlag(_CRTDBG_CHECK_ALWAYS_DF);
 #endif
 
+#ifdef COUNTERS_ON
+strcpy(counters_string,"\ncounters:");
+counters_mute=counters_string+strlen(counters_string);
+#else
+counters_string[0]=0;
+counters_mute=counters_string;
+#endif
+
+  
+
   if (argc==1)
     {
-    printf("usage: seg_int -aA -sS -dD -nN -pP -m\n");
+    printf("usage: seg_int -aA -sS -dD -nN -pP -m -c\n");
     printf("example: seg_int -t14 -sa -dp -n20000 -p5.5\n");
     printf("-aA: type of algorithms tested\n");
     printf(" A=1: trivial\n");
@@ -70,8 +83,9 @@ int main(int argc, char* argv[])
     printf(" A=4: balaban fast\n");
     printf(" A=8: balaban optimal\n");
     printf(" A=16: balaban fast parallel for 2 cores\n");
-    printf(" if you want several algorithms to alg just sum up their values\n");
-    printf(" i.e. A=31 (=1+2+4+8+16) all algorithms\n");
+    printf(" A=32: bentley & ottmann\n");
+    printf(" if you want several algorithms to test just sum up their values\n");
+    printf(" i.e. A=63 (=1+2+4+8+16+32) all algorithms\n");
     printf("-sS: type of segments\n");
     printf(" S=l: line segments representation y=a*x+b,x1<=x<=x2; a,b,x1,x2 - reals\n");
     printf(" S=L: line segments representation r=b+a*t,0<=t<=1; b,a - vectors\n");
@@ -85,6 +99,7 @@ int main(int argc, char* argv[])
     printf("-nN: number of segments\n");
     printf("-pP: parameter value, must be positive\n");
     printf("-m: if presented means 'mute' mode - few information printed\n");
+    printf("-c: counters are printed if presented\n");
 
     }
   else
@@ -97,7 +112,7 @@ int main(int argc, char* argv[])
           case 'a':
             {
             alg=atoi(argv[i]+2);
-            if((alg<1)||(alg>31))
+            if((alg<1)||(alg>63))
               {alg=4; printf("some error in -a param. 4 used instead.\n");}
             }
             break;
@@ -151,73 +166,80 @@ int main(int argc, char* argv[])
             mute=TRUE;
             }
             break;
+#ifdef COUNTERS_ON
+          case 'c':
+            {
+              pcs=counters_mute;
+            }
+            break;
+#endif
           }
         }
     }
   if(!mute)printf("actual param string is: -a%i -s%c -d%c -n%i -p%f\n", alg, ss[seg_type], sd[d], n, p);
   if((seg_type==arc)&&(d==mixed)) {printf("-sa -dm is not compartible!/n"); return 0;}
-  /*for(n=10000;n<150001;n+=10000)
-    {
-     double nint,et;
-     PSeg *coll=create_test_collection(seg_type,n,d,p);
-     et=_benchmark(n,coll,seg_type,fast,nint); 
-     delete_test_collection(seg_type,coll);
-         
-     printf("n%i;i%12.0f;t%5.2f;%5.3f;%5.3f;%5.3f\n",n,nint,et,counters[0],counters[1],counters[2]);
-    }
-  return 0;*/
+  
   PSeg *coll=create_test_collection(seg_type,n,d,p);
    
   if(alg&fast)
     {
-    exec_time[fast]=_benchmark(n,coll,seg_type,fast,nInt[fast]);
+    exec_time[fast]=_benchmark(pcs,n,coll,seg_type,fast,nInt[fast]);
     if(mute)
-      printf("a4;s%c;d%c;n%i;i%13.0f;t%8.2f;p%f\n",ss[seg_type], sd[d],n,nInt[fast],exec_time[fast],p);
+      printf("a4;s%c;d%c;n%i;i%13.0f;t%8.2f;p%f;%s\n",ss[seg_type], sd[d],n,nInt[fast],exec_time[fast],p,counters_mute);
     else
-      printf("fast   intersections=%13.0f time=%8.2f\n",nInt[fast],exec_time[fast]);
+      printf("fast   intersections=%13.0f time=%8.2f%s\n",nInt[fast],exec_time[fast],counters_string);
     }
 
   if(alg&optimal)
     {
-    exec_time[optimal]=_benchmark(n,coll,seg_type,optimal,nInt[optimal]);
+    exec_time[optimal]=_benchmark(pcs,n,coll,seg_type,optimal,nInt[optimal]);
     if(mute)
-      printf("a8;s%c;d%c;n%i;i%13.0f;t%8.2f;p%f\n",ss[seg_type], sd[d],n,nInt[optimal],exec_time[optimal],p);
+      printf("a8;s%c;d%c;n%i;i%13.0f;t%8.2f;p%f;%s\n",ss[seg_type], sd[d],n,nInt[optimal],exec_time[optimal],p,counters_mute);
     else
-      printf("optimal  intersections=%13.0f time=%8.2f\n",nInt[optimal],exec_time[optimal]);
+      printf("optimal  intersections=%13.0f time=%8.2f%s\n",nInt[optimal],exec_time[optimal],counters_string);
     }
 
   if(alg&fast_parallel)
     {
-    exec_time[fast_parallel]=_benchmark(n,coll,seg_type,fast_parallel,nInt[fast_parallel]);
+    exec_time[fast_parallel]=_benchmark(pcs,n,coll,seg_type,fast_parallel,nInt[fast_parallel]);
     if(mute)
-      printf("a16;s%c;d%c;n%i;i%13.0f;t%8.2f;p%f\n",ss[seg_type], sd[d],n,nInt[fast_parallel],exec_time[fast_parallel],p);
+      printf("a16;s%c;d%c;n%i;i%13.0f;t%8.2f;p%f;%s\n",ss[seg_type], sd[d],n,nInt[fast_parallel],exec_time[fast_parallel],p,counters_mute);
     else
-      printf("fast_parallel intersections=%13.0f time=%8.2f\n",nInt[fast_parallel],exec_time[fast_parallel]);
+      printf("fast_parallel intersections=%13.0f time=%8.2f%s\n",nInt[fast_parallel],exec_time[fast_parallel],counters_string);
+    }
+
+  if(alg&bentley_ottmann)
+    {  
+    exec_time[bentley_ottmann]=_benchmark(pcs,n,coll,seg_type,bentley_ottmann,nInt[bentley_ottmann]);
+    if(mute)
+      printf("a32;s%c;d%c;n%i;i%13.0f;t%8.2f;p%f;%s\n",ss[seg_type], sd[d],n,nInt[bentley_ottmann],exec_time[bentley_ottmann],p,counters_mute);
+    else
+      printf("bentley_ottmann intersections=%13.0f time=%8.2f%s\n",nInt[bentley_ottmann],exec_time[bentley_ottmann],counters_string);
     }
 
 
   if(alg&simple_sweep)
     {  
-    exec_time[simple_sweep]=_benchmark(n,coll,seg_type,simple_sweep,nInt[simple_sweep]);
+    exec_time[simple_sweep]=_benchmark(pcs,n,coll,seg_type,simple_sweep,nInt[simple_sweep]);
     if(mute)
-      printf("a2;s%c;d%c;n%i;i%13.0f;t%8.2f;p%f\n",ss[seg_type], sd[d],n,nInt[simple_sweep],exec_time[simple_sweep],p);
+      printf("a2;s%c;d%c;n%i;i%13.0f;t%8.2f;p%f;%s\n",ss[seg_type], sd[d],n,nInt[simple_sweep],exec_time[simple_sweep],p,counters_mute);
     else
-      printf("simple_sweep intersections=%13.0f time=%8.2f\n",nInt[simple_sweep],exec_time[simple_sweep]);
+      printf("simple_sweep intersections=%13.0f time=%8.2f%s\n",nInt[simple_sweep],exec_time[simple_sweep],counters_string);
     }
 
   if(alg&triv)
     {  
-    exec_time[triv]=_benchmark(n,coll,seg_type,triv,nInt[triv]);
+    exec_time[triv]=_benchmark(pcs,n,coll,seg_type,triv,nInt[triv]);
     if(mute)
-      printf("a1;s%c;d%c;n%i;i%13.0f;t%8.2f;p%f\n",ss[seg_type], sd[d],n,nInt[triv],exec_time[triv],p);
+      printf("a1;s%c;d%c;n%i;i%13.0f;t%8.2f;p%f;%s\n",ss[seg_type], sd[d],n,nInt[triv],exec_time[triv],p,counters_mute);
     else
-      printf("triv       intersections=%13.0f time=%8.2f\n",nInt[triv],exec_time[triv]);
+      printf("triv       intersections=%13.0f time=%8.2f%s\n",nInt[triv],exec_time[triv],counters_string);
     }
 
   /*printf("ratio fast  =%6.3f\n",0.5*(n*exec_time[fast]*(n-1))/(exec_time[triv]*nInt[fast]));*/
 
   delete_test_collection(seg_type,coll);
-  //getchar();
+  getchar();
   return 0;
   }
 
