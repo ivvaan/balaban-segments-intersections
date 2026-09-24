6e094595b50cf150fed8480043c31b6692c41853
ivvaan <ivvaan@gmail.com>
2026-09-24 15:53:14 +0400

    remove unnessesary bat

--- cl2cpp.bat
+++
@@ -1,5 +0,0 @@
-xcopy "C:\tmp\sint\degenerate_cases\*.h" "C:\cpp\segm_int\" /Y
-xcopy "C:\tmp\sint\degenerate_cases\*.cpp" "C:\cpp\segm_int\" /Y
-xcopy "C:\tmp\sint\degenerate_cases\*.md" "C:\cpp\segm_int\" /Y
-xcopy "C:\tmp\sint\degenerate_cases\*.html" "C:\cpp\segm_int\" /Y
-pause
--- cpp2cl.bat
+++
@@ -1,5 +0,0 @@
-xcopy "C:\cpp\segm_int\*.h" "C:\tmp\sint\degenerate_cases\" /Y
-xcopy "C:\cpp\segm_int\*.cpp" "C:\tmp\sint\degenerate_cases\" /Y
-xcopy "C:\cpp\segm_int\*.md" "C:\tmp\sint\degenerate_cases\" /Y
-xcopy "C:\cpp\segm_int\*.html" "C:\tmp\sint\degenerate_cases\" /Y
-pause
